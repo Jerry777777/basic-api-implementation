4bb79ede87f33134414175eae5e7b00384d405f7
Jerry777777 <491818175@qq.com>
2020-09-16 18:40:24 +0800

    [xiawenhao] feat: add user

--- src/main/java/com/thoughtworks/rslist/api/UserController.java
+++ src/main/java/com/thoughtworks/rslist/api/UserController.java
@@ -1,7 +1,25 @@
 package com.thoughtworks.rslist.api;
 
+import com.thoughtworks.rslist.domain.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
+    List<User> userList = new ArrayList<>();
+
+    @PostMapping("/rs/addUser")
+    public void addOne(@RequestBody User user){
+        userList.add(user);
+    }
+
+    @GetMapping("/rs/getUserList")
+    public List<User> getUserList(){
+        return userList;
+    }
 }
--- src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -1,11 +1,36 @@
 package com.thoughtworks.rslist.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.domain.Gender;
+import com.thoughtworks.rslist.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
 
+    @Test
+    void should_add_user() throws Exception {
+        User user = new User("zhangsan", Gender.MALE, 20, "a@b.com", "11234567890");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/rs/addUser").content(userString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/rs/getUserList"))
+                .andExpect(jsonPath("$.*", hasSize(1)));
+    }
 }
