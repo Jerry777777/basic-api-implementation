3fb531b44e707b7dc49cd052dd4604003bec1a17
Jerry777777 <491818175@qq.com>
2020-09-16 13:58:34 +0800

    [xiawenhao] feat: add init to reset before each test

--- src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -2,12 +2,14 @@ package com.thoughtworks.rslist.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.rslist.domain.RsEvent;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,6 +21,11 @@ import static org.hamcrest.Matchers.is;
 class RsControllerTest {
     @Autowired
     MockMvc mockMvc;
+
+    @BeforeEach
+    public void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new RsController()).build();
+    }
 
     @Test
     void should_get_list() throws Exception {
